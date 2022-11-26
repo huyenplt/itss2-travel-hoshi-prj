4b303abdc40ab81a2a749e06d4ccf5106f4e2a64
hoangviet15ht <hoangviet15ht@gmail.com>
2022-11-26 16:58:44 +0700

    add middleware

--- routes/user.php
+++ routes/user.php
@@ -14,19 +14,11 @@ use App\Http\Controllers\User\HomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [AuthController::class, 'userAuth']);
+Route::post('login', [AuthController::class, 'userLogin'])->name('login');
+Route::post('signup', [AuthController::class, 'userSignUp'])->name('signup');
 
-// Route::prefix('user')->group(function () {
-//     Route::get('/', [AuthController::class, 'userAuth'])->name('user');
-//     Route::post('login', [AuthController::class, 'userLogin'])->name('user.login');
-//     Route::post('signup', [AuthController::class, 'userSignUp'])->name('user.signup');
-//     Route::get('/home', [HomeController::class, 'index'])->name('user.home');
-// });
-
-Route::group([
-], function () {
-    Route::get('/', [AuthController::class, 'userAuth']);
-    Route::post('login', [AuthController::class, 'userLogin'])->name('login');
-    Route::post('signup', [AuthController::class, 'userSignUp'])->name('signup');
+Route::middleware(['role:user'])->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
 
