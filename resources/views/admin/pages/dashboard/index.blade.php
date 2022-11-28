4e7db9218c19dcec53fe39fae9d12ddddb081f73
huyenplt <huyen.phamlt19@gmail.com>
2022-11-29 02:01:59 +0700

    feat: update view all place page

--- resources/views/admin/pages/dashboard/index.blade.php
+++ resources/views/admin/pages/dashboard/index.blade.php
@@ -1,16 +1,24 @@
 @extends('admin.layout.app', ['activePage' => 'dashboard', 'title' => 'Dashboard', 'navName' => 'Dashboard', 'activeButton' => 'laravel'])
 
 @section('content')
-    <div class="content">
+    <div class="content dashboard">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="card card-plain table-plain-bg">
-                        <div class="card-header ">
-                            <h4 class="card-title">All places</h4>
+                    <div class="card strpied-tabled-with-hover">
+                        <div class="card-header">
+                            <div class="row">
+                                <h4 class="col card-title">All places {{ old('name', auth()->user()->name) }}</h4>
+                                <form class="col-lg-4 col-md-3 col-sm-5" method="" action="" autocomplete="off">
+                                    <div class="form-group dashboard-search d-flex align-items-center">
+                                        <i class="w3-xxlarge nc-icon nc-zoom-split dashboard-search__icon"></i>
+                                        <input type="text" name="" id="" class="form-control dashboard-search__input" placeholder="{{ __('Search') }}" autofocus>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                         <div class="card-body table-full-width table-responsive">
-                            <table class="table table-hover">
+                            <table class="table table-hover table-striped">
                                 <thead>
                                     <th>Address</th>
                                 </thead>
