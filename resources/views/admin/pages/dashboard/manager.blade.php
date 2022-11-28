0a531906cf7936eac726a95ee3019dc543db7ea7
TuanNH <tuannh7449@co-well.vn>
2022-11-28 19:08:33 +0700

    fix screen manager places

--- resources/views/admin/pages/dashboard/manager.blade.php
+++ resources/views/admin/pages/dashboard/manager.blade.php
@@ -2,10 +2,19 @@
 
 @section('content')
     <div class="content">
-        <div class="container-fluid">
-            @foreach ($places as $place)
-            {{$place->name}}
-            @endforeach
+        <div class="row">
+            <div class="col-sm">
+                <img src="https://deviet.vn/wp-content/uploads/2019/04/vuong-quoc-anh.jpg" />
+            </div>
+            <div class="col-sm">
+                <div class="p-4">
+                    <div class="border border-secondary border-bottom-0">
+                        @foreach ($places as $place)
+                            <div class="p-2 border-bottom">{{$place->name}}</div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
