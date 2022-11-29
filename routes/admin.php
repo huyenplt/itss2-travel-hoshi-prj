ec6b893535e462443b8404d36738a862e17652d9
TuanNH <tuannh7449@co-well.vn>
2022-11-29 09:57:26 +0700

    remove file places

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\BlogController;
-use App\Http\Controllers\Admin\PlaceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,14 +29,6 @@ Route::middleware(['role:admin'])->group(function () {
 
     Route::get('/users', [UserController::class, 'index'])->name('user');
     Route::get('/blogs', [BlogController::class, 'index'])->name('blog');
-
-    // place
-    Route::get('/places', [PlaceController::class, 'index'])->name('place.index');
-    Route::get('/places/create', [PlaceController::class, 'create'])->name('place.create');
-    Route::post('/places/create', [PlaceController::class, 'store'])->name('place.store');
-    Route::get('/places/{place}', [PlaceController::class, 'edit'])->name('place.edit');
-    Route::post('/places/{place}', [PlaceController::class, 'update'])->name('place.update');
-    Route::post('/places/delete/{place}', [PlaceController::class, 'delete'])->name('place.delete');
 
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
