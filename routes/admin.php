cb242122bdaa2f2d2e202eded9476bc7b7db69fa
hoangviet15ht <hoangviet15ht@gmail.com>
2022-12-01 00:49:58 +0700

    add create place

--- routes/admin.php
+++ routes/admin.php
@@ -27,6 +27,8 @@ Route::middleware(['role:admin'])->group(function () {
         Route::get('/detail/{id}', [DashboardController::class, 'detail'])->name('.detail');
         Route::get('/place/{id}', [DashboardController::class, 'place'])->name('.place');
         Route::get('/place/delete/{id}', [DashboardController::class, 'delete'])->name('.place.delete');
+        Route::get('/create', [DashboardController::class, 'create'])->name('.place.create');
+        Route::post('/store', [DashboardController::class, 'store'])->name('.place.store');
     });
 
     Route::get('/users', [UserController::class, 'index'])->name('user');
