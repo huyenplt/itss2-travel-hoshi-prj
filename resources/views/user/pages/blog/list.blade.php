c6bb8c4603de40b9ae8330594ccfb55f88bcb8e2
huyenplt <huyen.phamlt19@gmail.com>
2022-12-12 14:36:17 +0700

    place index : like & dislike btn

--- resources/views/user/pages/blog/list.blade.php
+++ resources/views/user/pages/blog/list.blade.php
@@ -12,20 +12,24 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1> hihi </h1>
+                <h1> {{ $place->countLikes() }} hihi </h1>
             </div> <!-- end of col -->
         </div> <!-- end of row -->
     </div> <!-- end of container -->
 </header> <!-- end of ex-header -->
 <!-- end of header -->
 
-<button class="btn btn-primary">Like</button>
-<button class="btn btn-primary">UnLike</button>
+@can('like', $place)
+<a href="{{ route('user.favourite.like', ['place' => $place]) }}" class="btn btn-primary">Like</a>
+@endcan
+@cannot('like', $place)
+<a href="{{ route('user.favourite.dislike', ['place' => $place]) }}" class="btn btn-primary">UnLike</a>
+@endcan
 
 
     <a href="#create-blog" class="btn-solid-lg page-scroll p-3 mr-2 popup-with-move-anim">+   Create new blog</a>
     <!-- Intro -->
-    <div id="intro" class="basic-1">
+    <div id="blog" class="basic-1">
         <h1>{{$place->name}}</h1>
         @foreach ($place->placeImages as $placeImage)
             <img class="place-img" src="{{asset($placeImage->file_path)}}" />
